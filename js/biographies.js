068618192d810492eb299e5e6b767ee81b9af2ff
Dave Voyles <Startingline13@hotmail.com>
2014-04-02 01:47:10 -0400

    Misc Additions
    - Centered on US now
    - Cleaned up code
    - Need to convert arrays to JSON
    - Need to add images for TEs on info window

---
+++ js/biographies.js
@@ -0,0 +1,16 @@
+﻿var locations = [
+    ["Dave Voyles",
+        "Philadelphia, PA",
+        39.964,
+        -75.162,
+        "I'm a Tech Evangelist at Microsoft, My goal is to build communities around writing the best games and applications possible, and providing the tools and knowledge to do so.",
+         "@DaveVoyles",
+         "www.DavidVoyles.wordpress.com"],
+    ["Jason Fox",
+        "Dallas, Texas",
+         32.80,
+        -96.80,
+        " am a Technical Evangelist for Microsoft. In my career, I’ve developed software, video games, repaired electronics and maintained automated missile systems in the US Navy. I’m always looking for great, new tech and love to build video games. Originally from Florida, but grew up and reside in Fort Worth, Texas.",
+        "@JasonGFox",
+        "www.jasongfox.com"]
+];
--- js/googlemaps.js
+++ js/googlemaps.js
@@ -2,24 +2,19 @@ $(function () {
 
     function initialize() {
 
-        var defaultLatlng = new google.maps.LatLng(-55.363882, 131.044922);
-
-        var locations = [
-            ["Dave Voyles",  -55.363882, 131.044922,  "Some stuff about me", "@DaveVoyles",   "www.CecilFielder.wordpress.com"],
-            ["Cecil Fielder", -55.363882, 141.044922, "Some stuff about me", "@CecilFielder", "www.CecilFielder.wordpress.com"]
-        ];
+        // Center of United States
+        var defaultLatlng = new google.maps.LatLng(37.09024, -95.712891);
         var biographies = [];
         var marker; // Used to have an i var here as well..... Still needed?
 
         // Default view for map
         var mapOptions = {
-            zoom: 4,
+            zoom: 5,
             center: defaultLatlng
         };
 
-        /* Draws content to map canvas */
+        // Draws content to map canvas 
         var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
 
         // Content for info window -- appears when user clicks on a marker
         var infowindow = new google.maps.InfoWindow({
@@ -33,13 +28,13 @@ $(function () {
         // Loop through each location
         for (i = 0; i < locations.length; i++) {
             marker = new google.maps.Marker({                                       // Create a new marker for each location in array        
-            position: new google.maps.LatLng(locations[i][1], locations[i][2]),     // Takes lat and lang as arguments
+            position: new google.maps.LatLng(locations[i][2], locations[i][3]),     // Takes lat and lang as arguments
             map: map,                                                               // Draws to this current map  
 
          });
 
             // Creates a bio for each person and stores it in an array
-            var biography = formatBiography(locations[i][0], locations[i][3], locations[i][4], locations[i][5]);
+            var biography = formatBiography(locations[i][0], locations[i][1], locations[i][4], locations[i][5], locations[i][6]);
             biographies.push(biography);
 
              
@@ -55,12 +50,13 @@ $(function () {
 
         // Formats text for bio -- apears above each pin when selected
         // Loops through content in "Locations" array and places it in bio
-        // Returns: A formatted string
-        function formatBiography(name, bodyContent, twitter, websiteUrl) {
+        // RETURNS: A formatted string
+        function formatBiography(name, city, bodyContent, twitter, websiteUrl) {
             var html = [""];
 
             html.push(
                 '<h1 id="firstHeading" class="firstHeading">' + name + '</h1>' +
+                '<h3>' + city + '</h3>' +
                 '<div id="bodyContent">' +
                     bodyContent +
                 '</div>' +
@@ -73,29 +69,6 @@ $(function () {
             return html.join('');
         };
 
-        //function drop() {
-        //    for (var i = 0; i < markerPoints.length; i++) {
-        //        setTimeout(function () {
-        //            addMarker();
-        //        }, i * 200);
-        //    }
-        //}
-
-        //function addMarker() {
-        //    markers.push(new google.maps.Marker({
-        //        position: neighborhoods[iterator],
-        //        map: map,
-        //        draggable: false,
-        //        animation: google.maps.Animation.DROP
-        //    }));
-        //    iterator++;
-        //}
-
-
-        // When a marker is clicked, open the info window for that marker
-        //google.maps.event.addListener(marker, 'click', function () {
-        //    infowindow.open(map, marker);
-        //});
     }
 
     // When dom load event is triggered, call initlaize function
