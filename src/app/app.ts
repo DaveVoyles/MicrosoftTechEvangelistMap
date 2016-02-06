cf34c97efa29ca3a9da065d4c7550591e82f8866
Jeremy Foster <jeremy.foster@live.com>
2016-02-06 10:20:14 -0800

    refactor app.html file, remove redirect to /login route, delete .js and .js.map files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,6 @@
 import { Component, provide } from "angular2/core";
 import { bootstrap } from "angular2/platform/browser";
-import { 
-	Router,
-	RouteConfig,
-	ROUTER_DIRECTIVES,
-	ROUTER_PROVIDERS,
-	LocationStrategy,
-	HashLocationStrategy
-} from "angular2/router";
+import { Router, RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy } from "angular2/router";
 import { HTTP_PROVIDERS } from "angular2/http";
 
 import { Login } from "../login/login";
@@ -15,30 +8,24 @@ import { Map } from "../map/map";
 import { AuthHelper } from "../authHelper/authHelper";
 
 @Component({
-	selector: "map",
-	template: "<router-outlet></router-outlet>",
-	directives: [ROUTER_DIRECTIVES],
-	providers: [HTTP_PROVIDERS]
+    selector: "map",
+    templateUrl: "src/app/app.html",
+    directives: [ROUTER_DIRECTIVES],
+    providers: [HTTP_PROVIDERS]
 })
 
 // Configure the routes for the app
 @RouteConfig([
-	{ name: "Login", component: Login, path: "/login" },
-	{ name: "Map", component: Map, path: "/map" }
+    { name: "Login", component: Login, path: "/login" },
+    { name: "Map", component: Map, path: "/map" }
 ])
 
 export class App {
-	constructor(router:Router, auth:AuthHelper) {
-		// Route the user to a view based on presence of access token
-		if (auth.access_token !== null) {
-			// access token exists...display the users files
-			router.navigate(["/Map"]);
-		}
-		else {
-			// access token doesn't exist, so the user needs to login
-			router.navigate(["/Login"]);
-		}
-	}
+    constructor(router: Router, auth: AuthHelper) {
+        router.navigate(["/Map"]);
+        // if we later want to navigate to the login page we use...
+        // router.navigate(["/Login"]);
+    }
 }
 
 bootstrap(App, [AuthHelper, ROUTER_PROVIDERS, provide(LocationStrategy, { useClass: HashLocationStrategy })]);
