5ecee2f72d5ec268e2e9ea8d089d3ca61029e738
Dave Voyles <Dave Voyles>
2016-02-06 17:39:36 -0500

    cleaned up typescript and added infobox for map

--- src/map/map.ts
+++ src/map/map.ts
@@ -11,11 +11,42 @@ import { AuthHelper      } from "../authHelper/authHelper"
 })
 
 export class Map {
-    private files               = [];
-    public bingAPIKey           ='Ah1_aJohnC76ttqxM-PjSm5rsabmFcLSOujmuYvfSmKSHAOk9Xm2X2E6AtCQBNPk';
+        
+    // Instantiate map & box
+    private myMap;
+    private defaultInfobox;
+    private pushpin;
+    
+    
+    // Locations
+    private aDefaultLngLat = [37.09024, -95.712891];    
+    private aCenterLoc     = new Microsoft.Maps.Location(this.aDefaultLngLat[0], this.aDefaultLngLat[1]);
+    
+    private infoboxOptions = { width: 400, height: 250, showCloseButton: true };  
+    private bingAPIKey     ='Ah1_aJohnC76ttqxM-PjSm5rsabmFcLSOujmuYvfSmKSHAOk9Xm2X2E6AtCQBNPk';
+    // private pushpin        = new Microsoft.Maps.Pushpin(this.aCenterLoc);         
+    
 
     constructor(http: Http, authHelper: AuthHelper) {
-		
+        
+     this.defaultInfobox = new Microsoft.Maps.Infobox(this.aCenterLoc, this.infoboxOptions);
+     this.myMap          = new Microsoft.Maps.Map(document.getElementById('BingMap'), {
+        credentials: this.bingAPIKey
+    }); 
+    this.pushpin         = new Microsoft.Maps.Pushpin(this.aCenterLoc);         
+        
+     // Center map on the United States   
+     this.myMap.setView({ zoom: 5, center: this.aCenterLoc })
+     
+     // Create pin and infobox
+     this.myMap.entities.clear();
+     this.myMap.entities.push(this.pushpin);
+     this.myMap.entities.push(this.defaultInfobox);  
+
+    //  this.defaultInfobox =  new Microsoft.Maps.Infobox(this.aCenterLoc, this.infoboxOptions);
+
+     // InfoBox to appear after user clicks on pin
+
         // Perform REST call into Microsoft Graph for files on OneDrive for Business
 		// http.get("https://graph.microsoft.com/v1.0/me/drive/root/children", {
 		// 	headers: new Headers({ "Authorization": "Bearer " + authHelper.access_token })
@@ -29,35 +60,23 @@ export class Map {
         //     });
 
         // Bing map instance w/ DV's API key
-        let map = new Microsoft.Maps.Map(document.getElementById('BingMap'), {
-            credentials: 'Ah1_aJohnC76ttqxM-PjSm5rsabmFcLSOujmuYvfSmKSHAOk9Xm2X2E6AtCQBNPk'
-        });
-               
-        
-        // Focus map on center of United States
-        let aDefaultLngLat = [37.09024, -95.712891];
-        
-        let aCenterLoc = new Microsoft.Maps.Location(aDefaultLngLat[0], aDefaultLngLat[1]);
-        map.setView({ zoom: 5, center: aCenterLoc })
+        // let map = new Microsoft.Maps.Map(document.getElementById('BingMap'), {
+        //     credentials: bingAPIKey
+        // });
+   
+    }    
+    
+   // Events
+    private pushpinClick = Microsoft.Maps.Events.addHandler(this.pushpin, 'click', ()=> this.defaultInfobox.setOptions({visible:false})); 
 
-        // Push pins 
-        map.entities.clear();
-        var pushpin = new Microsoft.Maps.Pushpin(aCenterLoc);
-        map.entities.push(pushpin);
-        
-        // InfoBox to appear after user clicks on pin
-        let infoboxOptions = { width: 500, height: 300 };
-        let defaultInfobox = new Microsoft.Maps.Infobox(aCenterLoc, infoboxOptions);
-        map.entities.push(defaultInfobox);
-    }  
-
-
+    
+         
     /***
      * Formats text for bio which appears above each pin when selected
      * Loops through content in "Locations" array and places it in bio. 
      * @return {string} 
      */
-    formatBiography(name, city, spec, img, bio, twitter, websiteUrl): string {
+   formatBiography(name, city, spec, img, bio, twitter, websiteUrl): string {
         var html = [""];
 
         html.push(
